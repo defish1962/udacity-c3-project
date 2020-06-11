1e56ee2ca57fac77eeebf3332237c72372ef4253
David Fish <60833879+defish1962@users.noreply.github.com>
2020-06-11 12:11:12 -0600

    Updates to K8s deployment files

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=udagramdfishdev1
-export POSTGRES_PASSWORD=SASpwd4udagram
-export POSTGRES_HOST=udagramdfishdev1.caamxfxdrwcq.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=udagramdfishdev1
+export POSTGRESS_USERNAME=udagramdfishdev1
+export POSTGRESS_PASSWORD=SASpwd4udagram
+export POSTGRESS_HOST=udagramdfishdev1.caamxfxdrwcq.us-east-2.rds.amazonaws.com
+export POSTGRESS_DB=udagramdfishdev1
 export AWS_BUCKET=udacity-c3-project-8589-9343-5643
 export AWS_REGION=us-east-2
 export AWS_PROFILE=default
